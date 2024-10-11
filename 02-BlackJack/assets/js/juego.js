d2e3fc7122d027893c0bde60e8d679f70a7f73b8
Periko_elmaki <pedro.sandoval.martinez@ces-vegamedia.es>
2024-10-11 18:35:18 +0200

    Seguimos optimizando

--- 02-BlackJack/assets/js/juego.js
+++ 02-BlackJack/assets/js/juego.js
@@ -1,69 +1,69 @@
 
 //  Función anónima autoinvocada, usamos
-(function() {
+(() => {
     // CUando unsamos función anónima, usamos el strict (restricciones al código para que sea seguro), BUENAS PRACTICAS
     'use strict'
     // DAría error si no declaramos variables. 
-    
-    
-    
+
+
+
     // creamos deck de cartas 
     let deck = [];
     // tipos de cartas que va a contener el deck 
-    const tipos      = ['C', 'D', 'H', 'S'],
-          especiales = ['A', 'Q','J','K'];
+    const tipos = ['C', 'D', 'H', 'S'],
+        especiales = ['A', 'Q', 'J', 'K'];
 
-     let puntosJugador = 0,
-        puntosCrupier = 0;
-    // let puntosJugadores = [];
+
+    let puntosJugadores = [];
 
     // Referencias de html 
-    const   btnPedir         = document.querySelector('#btnPedir'),
-            btnDetener       = document.querySelector('#btnDetener'),
-            btnNuevoJuego    = document.querySelector('#btnNuevo');
-   
-    const puntosHTML       = document.querySelectorAll('small'); //Selecciona los smalls de recuento
-   
-    const divCartasJugador = document.querySelector('#jugador-cartas'); //el div donde se pone la imagen
-    const divCartasCrupier = document.querySelector('#crupier-cartas');
+    const   btnPedir = document.querySelector('#btnPedir'),
+            btnDetener = document.querySelector('#btnDetener'),
+            btnNuevoJuego = document.querySelector('#btnNuevo');
+
+
+    const   divCartasJugadores = document.querySelectorAll('.divCartas'),
+            puntosHTML = document.querySelectorAll('small');
 
     // por defecto los jugadores son 1 
-    const inicializarJuego = ( numJugadores = 2 ) => {
-        crearDeck();
-        // Para ir creando jugadores, añadimos posiciones al array puntosJugadores
-        for( let i = 0; i< numJugadores; i++ ){
+    const inicializarJuego = (numJugadores = 2) => {
+        deck = crearDeck();
+
+        /*EN el array puntosJugadores, vamos a añadir posiciones con valor 0
+            que indican el número de jugadores*/
+        for (let i = 0; i < numJugadores; i++) {
             puntosJugadores.push(0);
         }
-        console.log({ puntosJugadores });
+
     }
 
     const crearDeck = () => {
         deck = [];
         // usamos un bucle que cree el string con cada letra 
-        for(let i=2; i<= 10; i++) {
+        for (let i = 2; i <= 10; i++) {
             // deck.push(i + 'C'); mejoramos eso
             // 'For of' va a ejecutar el bucle por cada tipo, cada valor q tiene ese array
-            for( let tipo of tipos )  { 
+            for (let tipo of tipos) {
                 deck.push(i + tipo);
             }
         }
 
         // Ptro for para las especiales 
-        for( let tipo of tipos ){
-            for( let esp of especiales){
+        for (let tipo of tipos) {
+            for (let esp of especiales) {
                 deck.push(esp + tipo);
             }
         }
         // hacemos uso de la libreria para desordenar el deck 
-        return  _.shuffle( deck );
+        return _.shuffle(deck);
     }
 
-   
+
 
     // Función pedir carta 
     const pedirCarta = () => {
         // controlamos que no haya más cartas 
-        if ( deck.length === 0 ){
+        if (deck.length === 0) {
             // muestra error en consola
             throw 'No hay cartas en la baraja';
         }
@@ -72,16 +72,16 @@
     }
 
     // Para sacar los puntos de cada carta 
-    const valorCarta = ( carta ) => {
+    const valorCarta = (carta) => {
         // necesitamos coger sólo el número de la carta, sin la letra 
         // los string se tratan como arrays
-        const valor = carta.substring(0, carta.length-1);
-        
+        const valor = carta.substring(0, carta.length - 1);
+
         //CONDICION TERNARIA. si es A vale 11, si es otra letra 10
-        return ( isNaN( valor ) ) ?             //primer if, si no es num 
-                ( valor === 'A' ) ? 11 : 10     // si es un As, 11, si es otra letra 10 
-                : valor * 1;                    //si no es letra, usamos el valor del num
-        
+        return (isNaN(valor)) ?             //primer if, si no es num 
+            (valor === 'A') ? 11 : 10     // si es un As, 11, si es otra letra 10 
+            : valor * 1;                    //si no es letra, usamos el valor del num
+
         // // funcion para comprobar si es numero o no 
         // if( isNaN( valor )){
         //     // no es numero 
@@ -89,77 +89,85 @@
         // }else {
         //     puntos = valor * 1; // fuerzas a que sea int  
         // }
+
+    }
+    // Turno: 0 = primer jugador, el último será el crupier
+    const acumularPuntos = (carta, turno) => {
+
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta(carta);
+        puntosHTML[turno].innerText = puntosJugadores[turno];
+
+        return puntosJugadores[turno];
+    }
+    
+    const crearCarta = ( carta, turno) => {
+        // mostramos carta
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/cartas/${carta}.png`;  //esto coge el nombre de la carta y busca imagen
+        // añadimos imgCarta a la clase de css
+        imgCarta.classList.add('carta');
         
-    }
+        divCartasJugadores[turno].append(imgCarta);
 
-    const acumularPuntos = () => {
 
     }
-    // Turno del pc 
     const turnoPC = (puntosMinimos) => {
-
-        do{
+        let puntosCrupier = 0;
+        do {
             const carta = pedirCarta();
+            // La última posicion de puntosjugador  es el pc
+            puntosCrupier = acumularPuntos(carta, puntosJugadores.length - 1);
+            crearCarta(carta, puntosJugadores.length - 1);
+            // // mostramos carta
+            // const imgCarta = document.createElement('img');
+            // imgCarta.src = `assets/cartas/cartas/${carta}.png`;  //esto coge el nombre de la carta y busca imagen
+            // // añadimos imgCarta a la clase de css
+            // imgCarta.classList.add('carta');
+            // divCartasCrupier.append(imgCarta);
 
-            puntosCrupier= puntosCrupier + valorCarta( carta );
-            puntosHTML[1].innerText = puntosCrupier;
-            
-            // tenemos que mostrar la carta 
-            const imgCarta = document.createElement('img');
-            imgCarta.src = `assets/cartas/cartas/${ carta }.png`;  //esto coge el nombre de la carta y busca imagen
-            // añadimos imgCarta a la clase de css
-            imgCarta.classList.add('carta');
-            divCartasCrupier.append( imgCarta );
-            
             // si esmayor a 21 ya ha perdido. terminamos
-            if( puntosMinimos >21 ){
+            if (puntosMinimos > 21) {
                 break;
             }
 
-        }while( (puntosCrupier < puntosMinimos) && ( puntosMinimos <=21 ) );
+        } while ((puntosCrupier < puntosMinimos) && (puntosMinimos <= 21));
 
         setTimeout(() => {
-            
-        
-        if( puntosCrupier === puntosMinimos){
-            alert('Nadie gana');
-        }else if ( puntosMinimos > 21){
-            alert('Crupier gana gay');
-        }else if( puntosCrupier > 21 ){
-            alert('EL jugador gana!!!!!!!!!!!!!');
-        }else
-            alert('Crupier gana');
 
-        }, 20 );
+
+            if (puntosCrupier === puntosMinimos) {
+                alert('Nadie gana');
+            } else if (puntosMinimos > 21) {
+                alert('Crupier gana gay');
+            } else if (puntosCrupier > 21) {
+                alert('EL jugador gana!!!!!!!!!!!!!');
+            } else
+                alert('Crupier gana');
+
+        }, 20);
     }
 
     // ++++++++++EVENTOS +++++++++++++
 
     // funcion con un callback (dentro otra funcion) esta es de flecha
     btnPedir.addEventListener('click', () => {
-        
+
         const carta = pedirCarta();
+        const puntosJugador = acumularPuntos(carta, 0);
 
-        puntosJugador = puntosJugador + valorCarta( carta );
-        puntosHTML[0].innerText = puntosJugador;
-        
-        // tenemos que mostrar la carta 
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/cartas/${ carta }.png`;  //esto coge el nombre de la carta y busca imagen
-        // añadimos imgCarta a la clase de css
-        imgCarta.classList.add('carta');
-        divCartasJugador.append( imgCarta );
+        crearCarta(carta, 0);
+   
 
-        if (puntosJugador > 21 ){
+        if (puntosJugador > 21) {
             console.warn('Mamaste');
-            btnPedir.disabled = true; 
-            btnDetener.disabled = true; 
-            turnoPC( puntosJugador ); //ahora va el pc
-        } else if (puntosJugador === 21 ){
+            btnPedir.disabled = true;
+            btnDetener.disabled = true;
+            turnoPC(puntosJugador); //ahora va el pc
+        } else if (puntosJugador === 21) {
             console.warn('21, GANASTE');
             btnPedir.disabled = true;
-            btnDetener.disabled = true; 
-            turnoPC( puntosJugador );
+            btnDetener.disabled = true;
+            turnoPC(puntosJugador);
         }
     })
 
@@ -167,24 +175,24 @@
     btnDetener.addEventListener('click', () => {
         btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoPC( puntosJugador );
+        turnoPC(puntosJugador);
     })
 
     btnNuevoJuego.addEventListener('click', () => {
+        inicializarJuego();
         btnPedir.disabled = false;
         btnDetener.disabled = false;
         // vaciamos deck 
         deck = [];
         deck = crearDeck();
-
         // Hay que reiniciar todos los puntos 
-        puntosJugador = 0;
-        puntosCrupier = 0;    
+        // puntosJugador = 0;
+        // puntosCrupier = 0;    
         puntosHTML[0].innerText = 0;
         puntosHTML[1].innerText = 0;
-        
-        divCartasCrupier.innerHTML = '';
-        divCartasJugador.innerHTML = '';
+
+       
+       
 
     })
 
